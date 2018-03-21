6cc655051b99e1def0d33e7328b2121cc2e0bd6b
Andriy Shcherbyak <andriyshcherbyak@gmail.com>
2018-03-21 07:56:43 +0200

    add function to get Api data

--- handler.js
+++ handler.js
@@ -1,23 +1,25 @@
 'use strict';
-
+const request = require('request');
 module.exports.getVenues = (event, context, callback) => {
 
 
 const inputs = inputsData(JSON.parse(event.body));
 
+foursquareApiData(inputs).then(function(data){
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Success!',
-      input: inputs,
+    message: data
     }),
   };
-
+  
   callback(null, response);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+})
+
+
+
 };
 
 
@@ -36,4 +38,57 @@ function inputsData (data) {
 		const expectedInput = 'please input correct data';
 		throw new Error('Incorrect input parameter: '+ err.message +'\nExpected:\n' + expectedInput);
 	}
+}
+
+//get data from Foursquare API
+
+
+ const foursquareApiData =  function(data) {
+
+  return new Promise (function(res,rej){
+      request({
+      url: 'https://api.foursquare.com/v2/venues/explore',
+      method: 'GET',
+      qs: {
+          client_id: 'IHPFHD2EUYEWT0SXBBCTY45W54JT4YPU2TTDYMGV5LEPCUUW',
+          client_secret: 'CUYH3G2JUNOOZOHD22MRRCH4VVYDCMCBHYEMSVGCIRVRHWDC',
+          ll: data.latitude + ',' + data.longitude,
+          radius: data.radius,
+          query: data.venueType,
+          v: currentDate()
+      }
+      }, (err, r, body) => {
+        err ? rej(err) : res(getFieldsFormApi(body));
+      });
+  
+  })
+  
+    
+  }
+
+  //get current date
+function currentDate() {
+  let date = new Date();
+
+  let dd = date.getDate();
+  if (dd < 10) dd = '0' + dd;
+
+  let mm = date.getMonth() + 1;
+  if (mm < 10) mm = '0' + mm;
+
+  var yy = date.getFullYear();
+  if (yy < 10) yy = '0' + yy;
+
+  return yy  + mm +  dd;
+}
+
+function getFieldsFormApi(bodyStr) {
+	const responseObj = JSON.parse(bodyStr);
+	return responseObj.response.groups[0].items.map(item => ({
+		Name: item.venue.name,
+		City: item.venue.location.city,
+		Street: item.venue.location.address,
+		Latitude: item.venue.location.lat,
+		Longitude: item.venue.location.lng
+	}))
 }
