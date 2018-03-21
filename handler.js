cf1319275529151c81971062b7d0a511e8d5bb2c
Andriy Shcherbyak <andriyshcherbyak@gmail.com>
2018-03-21 04:37:37 +0200

    add function to get inputs

--- handler.js
+++ handler.js
@@ -1,11 +1,16 @@
 'use strict';
 
 module.exports.getVenues = (event, context, callback) => {
+
+
+const inputs = inputsData(JSON.parse(event.body));
+
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: 'Success!',
+      input: inputs,
     }),
   };
 
@@ -14,3 +19,21 @@ module.exports.getVenues = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+
+// get inputs data
+
+function inputsData (data) {
+  try {
+    return {
+			venueType:  data.query,
+			radius:     data.radius,
+			latitude:   data.latitude,
+			longitude:  data.longitude
+		}
+  }
+  catch (err) {
+		const expectedInput = 'please input correct data';
+		throw new Error('Incorrect input parameter: '+ err.message +'\nExpected:\n' + expectedInput);
+	}
+}
