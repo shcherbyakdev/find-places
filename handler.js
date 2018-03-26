26502803295cde7e90a6c720e61467046b13fd17
Andriy Shcherbyak <andriyshcherbyak@gmail.com>
2018-03-26 18:02:05 +0300

    add eslint

--- handler.js
+++ handler.js
@@ -1,27 +1,25 @@
-'use strict';
+"use strict";
 
-const helpers = require('./helpers/helperFunctions');
+const helpers = require("./helpers/helperFunctions");
 
 module.exports.getVenues = (event, context, callback) => {
 
-try{
- const inputs = helpers.processInputsData(JSON.parse(event.body));
- helpers.getFoursquareApiData(inputs).then(function(data){
+    try {
+        const inputs = helpers.processInputsData(JSON.parse(event.body));
+        helpers.getFoursquareApiData(inputs).then(function(data){
+            const response = helpers.getResponseCSVVenue(data);
 
-const response = helpers.getResponseCSVVenue(data);
+            callback(null, response);
 
-  callback(null, response);
-
-})
-}catch(err) {
-  const response = {
-    statusCode: err.code || 500,
-    headers: { "Content-Type": "text/plain" },
-    body: String(err)
-  };
-  callback(null, response);
-}
-
+        })
+    }catch(err) {
+        const response = {
+            statusCode: err.code || 500,
+            headers: { "Content-Type": "text/plain" },
+            body: String(err)
+        };
+        callback(null, response);
+    }
 };
 
 
