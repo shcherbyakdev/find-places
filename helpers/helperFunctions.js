d7a0807d1e27d8a3c3cf1401f81c8d7af2023ce2
Andriy Shcherbyak <andriyshcherbyak@gmail.com>
2018-03-26 13:21:49 +0300

    Refactoring and rename functions

--- helpers/helperFunctions.js
+++ helpers/helperFunctions.js
@@ -1,4 +1,11 @@
-exports.currentDate = function(){
+
+const request = require('request');
+const validations = require('./validations');
+const config = require('../config/foursquare');
+
+const Json2csvParser = require('json2csv').Parser;
+
+ const getCurrentDate = function(){
     let date = new Date();
 
     let dd = date.getDate();
@@ -13,7 +20,7 @@ exports.currentDate = function(){
     return yy  + mm +  dd;
 }
 
-exports.getFieldsFormApi = function (bodyStr) {
+const getFieldsFormApi = function (bodyStr) {
     const responseObj = JSON.parse(bodyStr);
 	return responseObj.response.groups[0].items.map(item => ({
 		Name: item.venue.name,
@@ -23,3 +30,69 @@ exports.getFieldsFormApi = function (bodyStr) {
 		Longitude: item.venue.location.lng
 	}))
 }
+
+// get inputs data
+
+exports.processInputsData =  function (data) {
+    try {
+      return {
+              venueType:  validations.validateName(data.query),
+              radius:     validations.validateRadius(data.radius),
+              latitude:   validations.validatePoint(data.latitude),
+              longitude:  validations.validatePoint(data.longitude)
+      }
+      
+   
+    }
+    catch (err) {
+          const expectedInput = 'please input correct data';
+          throw new Error('Incorrect input parameter: '+ err.message +'\nExpected:\n' + expectedInput);
+      }
+  }
+  
+
+  //get data from Foursquare API
+
+
+ exports.getFoursquareApiData =  function(data) {
+
+    return new Promise (function(res,rej){
+        request({
+        url: 'https://api.foursquare.com/v2/venues/explore',
+        method: 'GET',
+        qs: {
+            client_id: config.CLIENT_ID,
+            client_secret: config.CLIENT_SECRET,
+            ll: data.latitude + ',' + data.longitude,
+           radius: data.radius,
+            query: data.venueType,
+            v: getCurrentDate()
+        }
+        }, (err, r, body) => {
+          err ? rej(err) : res(getFieldsFormApi(body));
+        });
+    
+    })
+    
+      
+    }
+    
+    exports.responseCsvVenue = function ( data ) {
+
+        const columnNames = ["Name", "City", "Street", "Latitude", "Longitude"];
+
+
+        const json2csvParser = new Json2csvParser({ columnNames,quote: ' ' });
+        const csv = json2csvParser.parse(data);
+        
+          const responseCsv = {
+            statusCode: 200,
+            headers: {'Content-Type' : 'text/csv; charset=utf-8'},
+            body: JSON.stringify({
+            message: csv
+            }),
+          };
+          
+          return responseCsv;
+
+    }
