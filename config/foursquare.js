400035df7129e87b86c66b522ef766d9f872e677
Andriy Shcherbyak <andriyshcherbyak@gmail.com>
2018-06-06 18:08:18 +0300

    change credentials

--- config/foursquare.js
+++ config/foursquare.js
@@ -1,4 +1,7 @@
+
+//It is not working now because I've changed all credentials in my AWS account. Feel free to download => change => deploy => and use your own lambda.
+
 module.exports = {
-    "CLIENT_ID": "IHPFHD2EUYEWT0SXBBCTY45W54JT4YPU2TTDYMGV5LEPCUUW",
-    "CLIENT_SECRET": "CUYH3G2JUNOOZOHD22MRRCH4VVYDCMCBHYEMSVGCIRVRHWDC"
+    "CLIENT_ID": "",
+    "CLIENT_SECRET": ""
 }
